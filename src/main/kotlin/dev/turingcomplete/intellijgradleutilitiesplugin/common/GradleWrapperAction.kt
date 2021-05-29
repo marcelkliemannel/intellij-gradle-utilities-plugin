26d61f06e357a4f1b69866f9eae6ce73f09faf6d
Marcel Kliemannel <dev@marcelkliemannel.com>
2021-05-29 17:48:50 +0200

    #9: Check if Gradle wrapper JAR checksum verification is configured

--- src/main/kotlin/dev/turingcomplete/intellijgradleutilitiesplugin/common/GradleWrapperAction.kt
+++ src/main/kotlin/dev/turingcomplete/intellijgradleutilitiesplugin/common/GradleWrapperAction.kt
@@ -2,7 +2,11 @@ package dev.turingcomplete.intellijgradleutilitiesplugin.common
 
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.util.NlsActions
+import com.intellij.openapi.util.PropertiesUtil
 import com.intellij.openapi.vfs.VirtualFile
+import java.io.FileReader
+import java.nio.file.Files
+import java.nio.file.Path
 import javax.swing.Icon
 
 abstract class GradleWrapperAction<R>(title: @NlsActions.ActionText String,
@@ -32,6 +36,40 @@ abstract class GradleWrapperAction<R>(title: @NlsActions.ActionText String,
     return projectDir.findFileByRelativePath("gradle/wrapper/gradle-wrapper.jar")
   }
 
+  protected fun readGradleWrapperProperties(projectDir: VirtualFile,
+                                            progressIndicator: ProgressIndicator,
+                                            handleError: (String, Exception) -> Unit) : List<Pair<String, String>> {
+
+    return GradleUtils.findGradleWrapperProperties(projectDir)?.let { gradleWrapperProperties ->
+      readProperties("Reading Gradle wrapper properties...", gradleWrapperProperties.toNioPath(), progressIndicator, handleError)
+    } ?: listOf()
+  }
+
   // -- Private Methods --------------------------------------------------------------------------------------------- //
+
+  protected fun readProperties(progressText: String,
+                               propertiesFile: Path,
+                               progressIndicator: ProgressIndicator,
+                               handleError: (String, Exception) -> Unit): List<Pair<String, String>> {
+
+    progressIndicator.checkCanceled()
+
+    try {
+      progressIndicator.text = progressText
+
+      if (!Files.exists(propertiesFile)) {
+        return listOf()
+      }
+
+      return FileReader(propertiesFile.toFile()).use {
+        PropertiesUtil.loadProperties(it).map { property -> Pair(property.key, property.value) }.sortedBy { it.first }
+      }
+    }
+    catch (e: Exception) {
+      handleError("Failed to read properties file: $propertiesFile", e)
+      return listOf()
+    }
+  }
+
   // -- Inner Type -------------------------------------------------------------------------------------------------- //
 }
